ccef1a1ba91d38f693818dc9d04aa5db794618b3
Kyle Crouse <kyle.crouse@gmail.com>
2021-11-03 22:34:30 +0000

    blocking seasons by year

--- src/components/seasons.js
+++ src/components/seasons.js
@@ -3,29 +3,40 @@ import SeasonCard from './seasonCard'
 import './seasons.css'
 
 const Seasons = (props) => {
-	return (
-		<div class="columns">
-			{ props.seasons.map(season => {
-				season.standings = season.standings
-					.slice(0, 3)
-					.map(item => ({
-						...item,
-						driver: props.drivers.find(
-							({ name }) => name === item.driver
+	let seasons = {}
+	props.seasons.forEach(season => {
+		const [, year] = season.name.match(/Output Racing (\d+)/);
+		if (seasons[year])
+			seasons[year].push(season)
+		else
+			seasons[year] = [season]
+	})
+
+	return Object.entries(seasons)
+		.sort((a, b) => b[0] - a[0])
+		.map(([years, seasons]) => (
+			<div class="columns">
+				{ seasons.map(season => {
+						season.standings = season.standings
+							.slice(0, 3)
+							.map(item => ({
+								...item,
+								driver: props.drivers.find(
+									({ name }) => name === item.driver
+								)
+							}))
+						season.cars = season.cars.map(
+							name => props.cars.find(car => car.name === name)
 						)
-					}))
-				season.cars = season.cars.map(
-					name => props.cars.find(car => car.name === name)
-				)
-				return (
-					<div class="col-4 season-container">
-						<SeasonCard {...season} path={props.path}/>
-					</div>
-				)
-			})
-		}
-	</div>
-	)
+						return (
+							<div class="col-4 season-container">
+								<SeasonCard {...season} path={props.path}/>
+							</div>
+						)
+					})
+				}
+			</div>
+		))
 }
 
 export default Seasons
