a98f39bb8583571c39b1bc55ff982db78b90dbcc
Kyle Crouse <kylecrouse@users.noreply.github.com>
2023-02-22 19:02:45 +0000

    Updated rules for 2023

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -4,11 +4,11 @@ import { BLOCKS, MARKS } from "@contentful/rich-text-types"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
 import Layout from '../components/layout'
 import Meta from '../components/meta'
-import Tabs from '../components/tabs'
+// import Tabs from '../components/tabs'
 import * as styles from './rules.module.css'
 
 const Bold = ({ children }) => <span className="bold">{children}</span>
-const Text = ({ children }) => <p className="align-center">{children}</p>
+const Text = ({ children }) => <pre>{children}</pre>
 
 const options = {
 	renderMark: {
@@ -18,6 +18,11 @@ const options = {
 		[BLOCKS.HEADING_3]: (node, children) => {
 			return (
 				<h3 id={ `section-${node.content[0].value.replace(/\s/g, '-').toLowerCase()}` }>{ children }</h3>
+			)
+		},
+		[BLOCKS.HEADING_4]: (node, children) => {
+			return (
+				<h4 id={ `section-${node.content[0].value.replace(/\s/g, '-').toLowerCase()}` }>{ children }</h4>
 			)
 		},
 		[BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
@@ -46,96 +51,31 @@ const RulesPage = (props) => {
 							<h2 className="page-title">Rulebook</h2>
 						</hgroup>
 					
-						<Tabs 
-							defaultIndex={getDefaultTabIndex(props.location.hash)}
-							onSelect={
-								(index) => {
-									switch (index) {
-										case 1:
-											window.history.pushState(null, null, '#session-info')
-											return true
-										case 2:
-											window.history.pushState(null, null, '#code-of-conduct')
-											return true
-										default:
-											window.history.pushState(null, null, '#rules')
-											return true
+						<div className="columns" style={{ marginTop: 0 }}>
+							<div className="column col-3 hide-sm">
+								<ol className={ styles.toc }>
+									{ React.useMemo(
+											() => (
+												(JSON.parse(props.data.league.rules.raw)).content[0].content
+													// .filter(({ nodeType }) => nodeType === 'heading-3')
+													.map(({ content }) => content[0].content[0].value)
+													.map(section => (
+														<li key={section}>
+															<Link to={`#section-${section.replace(/\s/g,'-').toLowerCase()}`}>
+																{ section }
+															</Link>
+														</li>
+													))
+											),
+											[props.data.league.rules.raw]
+										)		
 									}
-								}
-							}
-							tabs={[
-								{
-									title: 'Rules',
-									content: (
-										<div className="columns">
-											<div className="column col-4 hide-sm">
-												<ol className={ styles.toc }>
-													{ React.useMemo(
-															() => (
-																(JSON.parse(props.data.league.rules.raw)).content
-																	.filter(({ nodeType }) => nodeType === 'heading-3')
-																	.map(({ content }) => content[0].value)
-																	.map(section => (
-																		<li>
-																			<Link to={`#section-${section.replace(/\s/g,'-').toLowerCase()}`}>
-																				{ section }
-																			</Link>
-																		</li>
-																	))
-															),
-															[props.data.league.rules.raw]
-														)		
-													}
-												</ol>
-											</div>
-											<div className="column col-6 col-sm-12 col-mx-auto">
-												{ renderRichText(props.data.league.rules, options) }
-											</div>
-										</div>
-									),
-								},
-								{
-									title: 'Session Info',
-									content: (
-										<div className="columns">
-											<div className="column col-4 hide-sm">
-												<ol className={ styles.toc }>
-													{ React.useMemo(
-															() => (
-																(JSON.parse(props.data.league.raceInfo.raw)).content
-																	.filter(({ nodeType }) => nodeType === 'heading-3')
-																	.map(({ content }) => content[0].value)
-																	.map(section => (
-																		<li>
-																			<Link to={`#section-${section.replace(/\s/g,'-').toLowerCase()}`}>
-																				{ section }
-																			</Link>
-																		</li>
-																	))
-															),
-															[props.data.league.raceInfo.raw]
-														)		
-													}
-												</ol>												
-											</div>
-											<div className="column col-6 col-sm-12 col-mx-auto">
-												{ renderRichText(props.data.league.raceInfo, options) }
-											</div>
-										</div>
-									),
-								},
-								{
-									title: 'Code of Conduct',
-									content: (
-										<div className="columns">
-											<div className="column col-6 col-sm-12 col-mx-auto">
-												{ renderRichText(props.data.league.codeOfConduct, options) }
-											</div>
-										</div>
-									),
-								},
-							]}
-						/>
+								</ol>
+							</div>
+							<div className={`${styles.body} column col-8 col-sm-12 col-mx-auto`}>
+								{ renderRichText(props.data.league.rules, options) }
+							</div>
+						</div>
 	
 					</div>
 				</div>
@@ -145,28 +85,22 @@ const RulesPage = (props) => {
 	)
 }
 
-const getDefaultTabIndex = (hash) => {
-	switch (hash) {
-		case "#session-info":
-			return 1
-		case "#code-of-conduct":
-			return 2
-		default:
-			return 0
-	}
-}
+// const getDefaultTabIndex = (hash) => {
+// 	switch (hash) {
+// 		case "#session-info":
+// 			return 1
+// 		case "#code-of-conduct":
+// 			return 2
+// 		default:
+// 			return 0
+// 	}
+// }
 
 export const query = graphql`
 	query RulesQuery {
 		league: contentfulLeague(leagueId: {eq: 2732}) {
 			name
 			rules {
-				raw
-			}
-			raceInfo {
-				raw
-			}
-			codeOfConduct {
 				raw
 			}
 		}
